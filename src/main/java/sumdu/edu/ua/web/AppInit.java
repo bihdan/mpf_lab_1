55914a4dfe910cac7a80131523f3245ac155fa43
Bohdan Dovhopolov <bogdandovgopolov2004@gmail.com>
2026-01-12 02:27:14 +0200

    lab3

--- src/main/java/sumdu/edu/ua/web/AppInit.java
+++
@@ -1,8 +0,0 @@
-package sumdu.edu.ua.web;
-
-public class AppInit implements jakarta.servlet.ServletContextListener {
-    @Override public void contextInitialized(jakarta.servlet.ServletContextEvent e) {
-        try { new CommentDao().init(); }
-        catch (Exception ex) { throw new RuntimeException(ex); }
-    }
-}
